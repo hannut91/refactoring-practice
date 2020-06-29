34c5385e4454478a2fab12f340049a84e2cc6d68
hannut91 <hannut91@gmail.com>
2020-06-29 13:37:22 +0900

    Add Encpsulate Variable Refactoring example
    Add Encpsulate Variable Refactoring example.

---
+++ 6.6-encapsulate-variable/app.js
@@ -0,0 +1,4 @@
+export const defaultOwner = {
+  firstName: '마틴',
+  lastName: '파울러',
+};
---
+++ 6.6-encapsulate-variable/app.test.js
@@ -0,0 +1,31 @@
+import { defaultOwner } from './app';
+
+describe('Get defaultOwner', () => {
+  it('has name', () => {
+    expect(defaultOwner).not.toBeUndefined();
+    expect(defaultOwner.firstName).not.toBeUndefined();
+    expect(defaultOwner.firstName).not.toBeUndefined();
+  });
+
+  context.skip('when change field', () => {
+    it('does not change field', () => {
+      const originFirstName = defaultOwner.firstName;
+      defaultOwner.firstName = '켄트';
+
+      expect(defaultOwner).not.toBeUndefined();
+      expect(defaultOwner.firstName).toBe(originFirstName);
+    });
+  });
+});
+
+describe.skip('Set defaultOwner', () => {
+  it('sets defaultOwner', () => {
+    defaultOwner = {
+      firstName: '켄트',
+      lastName: '백',
+    };
+
+    expect(defaultOwner.firstName).toBe('켄트');
+    expect(defaultOwner.lastName).toBe('백');
+  });
+});
