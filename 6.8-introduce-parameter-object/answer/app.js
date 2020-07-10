b05bd847819ff8c5df5379dd689bc4eabc9f2c5b
hannut91 <hannut91@gmail.com>
2020-07-10 13:04:49 +0900

    Replace compairing values to contains of range

--- 6.8-introduce-parameter-object/answer/app.js
+++ 6.8-introduce-parameter-object/answer/app.js
@@ -1,3 +1,3 @@
 export default function readingsOutsideRange(station, range) {
-  return station.readings.filter((r) => r.temp < range.min || r.temp > range.max);
+  return station.readings.filter((r) => !range.contains(r.temp));
 }
