a7f8d28de5e753960f49928c474a3cd6ace4e515
hannut91 <hannut91@gmail.com>
2020-08-04 19:14:53 +0900

    Copy file

---
+++ 6.9-combine-functions-into-class/answer/app.test.js
@@ -0,0 +1,59 @@
+const taxThreshold = (year) => 500;
+
+const baseRate = (month, year) => 1000;
+
+const baseCharge = (aReading) => baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+const acquireReading = () => ({
+  customer: 'ivan',
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
+
+class Reading {
+  constructor({
+    customer, quantity, month, year,
+  }) {
+    this.customer = customer;
+    this.quantity = quantity;
+    this.month = month;
+    this.year = year;
+  }
+}
+
+describe('TeaCharge', () => {
+  describe('client 1', () => {
+    it('returns calcualted base charge', () => {
+      const aReading = acquireReading();
+      const charge = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+
+      expect(charge).toBe(10000);
+    });
+  });
+
+  describe('client 2', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const base = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+      const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+      expect(taxableCharge).toBe(9500);
+    });
+  });
+
+  describe('client 3', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const charge = calculateBaseCharge(aReading);
+
+      function calculateBaseCharge(aReading) {
+        return baseRate(aReading.month, aReading.year) * aReading.quantity;
+      }
+
+      expect(charge).toBe(10000);
+    });
+  });
+});
