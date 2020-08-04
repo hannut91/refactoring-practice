5402fd8a23c3222c88fb8d895688ed6e7e8b7bc1
hannut91 <hannut91@gmail.com>
2020-08-04 19:08:28 +0900

    Add 6.9 Combine functions into class refactoring
    Add 6.9 Combine functions into class refactoring.
    
    See also
      - https://refactoring.com/catalog/combineFunctionsIntoClass.html

---
+++ 6.9-combine-functions-into-class/app.test.js
@@ -0,0 +1,49 @@
+const taxThreshold = (year) => 500;
+
+const baseRate = (month, year) => 1000;
+
+const baseCharge = (aReading) =>
+  baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+const acquireReading = () => ({
+  customer: 'ivan',
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
+
+describe('TeaCharge', () => {
+  describe('client 1', () => {
+    it('returns calcualted base charge', () => {
+      const aReading = acquireReading();
+      const charge = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+
+      expect(charge).toBe(10000);
+    });
+  });
+
+  describe('client2', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const base = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+      const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+      expect(taxableCharge).toBe(9500);
+    });
+  });
+
+  describe('client3', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const charge = calculateBaseCharge(aReading);
+
+      function calculateBaseCharge(aReading) {
+        return baseRate(aReading.month, aReading.year) * aReading.quantity;
+      }
+
+      expect(charge).toBe(10000);
+    });
+  });
+});
