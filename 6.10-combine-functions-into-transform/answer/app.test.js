a80831d22b2d8cdbcb7b71c9bcd82ccbf617c562
hannut91 <hannut91@gmail.com>
2020-08-06 20:12:49 +0900

    Copy origin code

---
+++ 6.10-combine-functions-into-transform/answer/app.test.js
@@ -0,0 +1,46 @@
+const taxThreshold = () => 500;
+
+const baseRate = () => 1000;
+
+const calculateBaseCharge = (aReading) => (
+  baseRate(aReading.month, aReading.year) * aReading.quantity
+);
+
+const acquireReading = () => ({
+  customer: 'ivan',
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
+
+describe('TeaCharge', () => {
+  describe('client 1', () => {
+    it('returns calcualted base charge', () => {
+      const aReading = acquireReading();
+      const charge = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+
+      expect(charge).toBe(10000);
+    });
+  });
+
+  describe('client2', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const base = baseRate(aReading.month, aReading.year)
+        * aReading.quantity;
+      const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+      expect(taxableCharge).toBe(9500);
+    });
+  });
+
+  describe('client3', () => {
+    it('returns calculated charge with tax', () => {
+      const aReading = acquireReading();
+      const charge = calculateBaseCharge(aReading);
+
+      expect(charge).toBe(10000);
+    });
+  });
+});
